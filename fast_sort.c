56d1f05063b1072690e4315a55947f2cbb982692
danormalnaya <92042398+danormalnaya@users.noreply.github.com>
2022-04-02 17:02:24 +0300

    Add files via upload

--- fast_sort.c
+++ fast_sort.c
@@ -6,7 +6,7 @@
 /*   By: lloko <lloko@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 18:48:18 by lloko             #+#    #+#             */
-/*   Updated: 2022/03/27 19:12:57 by lloko            ###   ########.fr       */
+/*   Updated: 2022/04/02 16:09:04 by lloko            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,86 @@
 
 static void	sort_2_items(t_stack **stack)
 {
-	if (*stack)->next->index < (*stack)->index)
-		swap(stack, a);
+	if ((*stack)->next->index < (*stack)->index)
+		swap(stack, 'a');
 }
 
 static void	sort_3_items(t_stack **stack)
 {
 	if ((*stack)->index == max_index(*stack))
-		swap(stack, a);
-	if ((*stack)->index == min_index(*stack) && (*stack)->next->index == max_index(*stack))
+		swap(stack, 'a');
+	if ((*stack)->index == min_index(*stack)
+		&& (*stack)->next->index == max_index(*stack))
 	{
-		rr(stack, a);
-		swap(stack, a);
+		rr(stack, 'a');
+		swap(stack, 'a');
 	}
-	if ((*stack)->index != min_index(*stack) && (*stack)->index > (*stack)->next->index)
-		swap(stack, a);
-	if ((*stack)->index != min_index(*stack) && (*stack)->index < (*stack)->next->index)
-		rr(stack, a);
+	if ((*stack)->index != min_index(*stack)
+		&& (*stack)->index > (*stack)->next->index)
+		swap(stack, 'a');
+	if ((*stack)->index != min_index(*stack)
+		&& (*stack)->index < (*stack)->next->index)
+		rr(stack, 'a');
 }
 
-static void	sort_4_items(t_stack **stack)
+static void	sort_4_items(t_stack **a, t_stack **b)
+{
+	int	i;
+
+	i = 0;
+	while (i < 2)
+	{
+		if ((*a)->index > min_index(*a)
+			&& !compare(a, min_index(*a), stack_len(*a)))
+			r(a, 'a');
+		else if ((*a)->index > min_index(*a)
+			&& compare(a, min_index(*a), stack_len(*a)))
+			rr(a, 'a');
+		else
+		{
+			p(a, b, 'b');
+			i++;
+		}
+	}
+	if ((*a)->index != min_index(*a))
+		s(a, 'a');
+	while (*b)
+		p(a, b, 'a');
+}
+
+static void	sort_5_items(t_stack **a, t_stack **b)
+{
+	int	i;
+
+	i = 0;
+	while (i < 3)
+	{
+		if ((*a)->index > min_index(*a)
+			&& !compare(a, min_index(*a), stack_len(*a)))
+			r(a, 'a');
+		else if ((*a)->index > min_index(*a)
+			&& compare(a, min_index(*a), stack_len(*a)))
+			rr(a, 'a');
+		else
+		{
+			p(a, b, 'b');
+			i++;
+		}
+	}
+	if ((*a)->index != min_index(*a))
+		s(a, 'a');
+	while (*b)
+		p(a, b, 'a');
+}
+
+void	sotring_up_to_5(t_stack **a, t_stack **b, int size)
+{
+	if (size == 2)
+		sort_2_items(a);
+	else if (size == 3)
+		sort_3_items(a);
+	else if (size == 4)
+		sort_4_items(a, b);
+	else if (size == 5)
+		sort_5_items(a, b);
+}
