16d30e25de9ff850ef8a5a1c44e1d4ec97d4509e
danormalnaya <92042398+danormalnaya@users.noreply.github.com>
2022-04-02 17:07:49 +0300

    Add files via upload

---
+++ from_libft.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   from_libft.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lloko <lloko@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/27 17:36:17 by lloko             #+#    #+#             */
+/*   Updated: 2022/04/02 17:07:08 by lloko            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	if (fd > 0)
+	{
+		while (s[i] != '\0')
+		{
+			ft_putchar_fd(s[i], fd);
+			i++;
+		}
+		write(fd, "\n", 1);
+	}
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	*s;
+
+	if (fd > 0)
+	{
+		s = ft_itoa(n);
+		ft_putstr_fd(s, fd);
+		free(s);
+	}
+}
+
+void	ft_putchar_fd(char c, int fd)
+{
+	if (fd > 0)
+		write(fd, &c, 1);
+}
